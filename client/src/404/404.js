291e3a01b94bfe1e3208e34c02680e080ea93793
idriss30 <idriss.cissoko@hotmail.com>
2023-02-10 17:32:43 -0500

    replaced anchor tag with Link from react router

--- client/src/404/404.js
+++ client/src/404/404.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./404.scss";
+import { Link } from "react-router-dom";
 
 function ErrorPage() {
   return (
@@ -7,7 +8,7 @@ function ErrorPage() {
       <div className="error__section__title">
         <h1>Nothing to see Here.</h1>
         <p>
-          Please consider visiting the <a href="/">Home</a> Page
+          Please consider visiting the <Link to="/">Home</Link> Page
         </p>
       </div>
 
