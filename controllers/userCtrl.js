5f44d278ae0f0975c44c1ad69c9d59d9fcd253dd
idriss30 <idriss.cissoko@hotmail.com>
2023-02-23 22:25:37 -0500

    refactoring controller

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,23 +1,27 @@
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-module.exports.postLogin = (req, res, next) => {
+module.exports.postLogin = async (req, res, next) => {
   /// extract userData from the body
   const userData = { ...req.body };
-  if (userData) {
-    // destructure data
-    const { ...user } = { ...userData.userData };
-    //check if user exist in database
-    User.findOne({ username: user.username }, (err, databaseUser) => {
-      if (err) return console.error(err);
-      if (!databaseUser) {
-        res.json({ error: "can't find user" });
-      } else {
-        databaseUser.password === user.password
-          ? res.json({ databaseUser })
-          : res.json({ error: "password don't match" });
-      }
-    });
+  // destructure data
+  const { username, password } = { ...userData };
+  const databaseUser = await User.findOne({ username });
+  if (!databaseUser) return res.status(404).json({ message: "user not found" });
+  if (databaseUser.password === password) {
+    jwt.sign({ username }, process.env.PASSPHRASE, { expiresIn: "1h" });
+    return res.status(200).json({ message: "login in" });
   }
+  return res.status(403).json({ message: "password not found" });
 };
 
-module.exports.logout = (req, res) => {};
+module.exports.checkToken = (req, res, next) => {
+  try {
+    const token = req.token;
+    const decoded = jwt.verify(token, process.env.PASSPHRASE);
+    return res.json({ username: decoded.username });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
