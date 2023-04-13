07a2f5457cee2defe4e85c63ad436139cc3b9eb1
idriss30 <idriss.cissoko@hotmail.com>
2023-04-12 23:57:25 -0400

    changed basename to landcare

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <BrowserRouter>
+        <BrowserRouter basename="/landcare">
           <NavBar />
           <Switch>
             <Route exact path="/">
